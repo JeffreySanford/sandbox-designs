a2f70b456be2ee6cfb2d405b02160433f5ded8e3
Jeffrey Sanford <jeffreysanford@gmail.com>
2015-12-26 15:40:28 -0800

    working on ravenwoodstudios - added array based and abject based methods in express framework.  WOrking - next dev in ID based

---
+++ express/ravenwoodstudios/objects/public/javascripts/javascript.js
@@ -0,0 +1,9 @@
+/*global window */
+(function IFEE() {
+    'use strict';
+    //Global Variables
+
+    window.onload = function onLoadEvent() {
+        console.log("window loaded");
+    };
+}());
