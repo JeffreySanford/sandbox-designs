9d7d72084d5e677e4673b4060c7ae0d8368148a7
Jeffrey Sanford <jeffreysanford@gmail.com>
2015-12-27 12:24:13 -0800

    init commit

--- express/ravenwoodstudios/prod-openshift/app.js
+++ express/ravenwoodstudios/prod-openshift/app.js
@@ -1,30 +1,60 @@
-(function IFEE() {
-    'use strict';
-    var express = require('express');
-    var app = express();
-    var routes = require('./routes');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-    app.use(express.static(__dirname + '/public'));
-    app.use(express.static(__dirname + '/data'));
-    app.use(express.static(__dirname + '/images'));
-    app.use(express.static(__dirname + '/stylesheets'));
-    app.use(express.static(__dirname + '/javascript'));
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-    app.set('view engine', 'ejs');
-    app.set('views', __dirname + '/views');  // not required if default views
+var app = express();
 
-    app.get('/', routes.index);
-    app.get('/about', routes.about);
-    app.get('/products', routes.products);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-    app.get('*', function fail(req, res) {
-        res.send('bad route found.');
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
-        
-    var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-    var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+  });
+}
 
-    var server = app.listen(server_port, server_ip_address, function () {
-        console.log("Listening on " + server_ip_address + ", server_port " + server_port);
-    });
-}());
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
