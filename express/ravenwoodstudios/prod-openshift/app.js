3d274dd6e427f9de33ac04e578ab511adde352d0
Jeffrey Sanford <jeffreysanford@gmail.com>
2015-12-27 00:03:37 -0800

    server

--- express/ravenwoodstudios/prod-openshift/app.js
+++ express/ravenwoodstudios/prod-openshift/app.js
@@ -22,7 +22,14 @@
     });
 
 
-    var server = app.listen(3000, function listenFunct() {
-        console.log('Server on port 3000');
+//    var server = app.listen(3000, function listenFunct() {
+//        console.log('Server on port 3000');
+        
+    var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+    var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+    server.listen(server_port, server_ip_address, function () {
+    console.log( "Listening on " + server_ip_address + ", server_port " + port )
+});
     });
 }());
