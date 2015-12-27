b6ae94dc4de8c9986d83b17d5074770218666bf6
Jeffrey Sanford <jeffreysanford@gmail.com>
2015-12-27 15:01:40 -0800

    push to heroku

--- express/ravenwoodstudios/prod-openshift/app.js
+++ express/ravenwoodstudios/prod-openshift/app.js
@@ -1,58 +1,28 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var routes = require('./routes/index');
-var users = require('./routes/users');
+(function IFEE() {
+    'use strict';
+    var express = require('express');
+    var app = express();
+    var routes = require('./routes');
 
-var app = express();
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + '/data'));
+    app.use(express.static(__dirname + '/images'));
+    app.use(express.static(__dirname + '/stylesheets'));
+    app.use(express.static(__dirname + '/javascript'));
+    
+    app.set('view engine', 'ejs');
+    app.set('views', __dirname + '/views');  // not required if default views
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+    app.get('/', routes.index);
+    app.get('/about', routes.about);
+    app.get('/products', routes.products);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.get('*', function fail(req, res) {
+        res.send('bad route found.');
     });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
 
 
-module.exports = app;
+    var server = app.listen(3000, function listenFunct() {
+        console.log('Server on port 3000');
+    });
+}());
